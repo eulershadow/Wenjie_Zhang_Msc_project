28fe8c6be52bc5416cd8b6d8536af09c573d52f4
eulershadow <zwj1033021@163.com>
2024-03-18 15:34:47 +0000

    point net paper

---
+++ code/data_process_ml.py
@@ -0,0 +1,10 @@
+import matplotlib as plt
+import sklearn as sklearn
+import scipy as scipy
+import pandas as pd
+import numpy as np
+
+
+training_data_path = ""
+data = pd.read_csv(training_data_path)
+target_data = pd.read_csv(training_data_path)
